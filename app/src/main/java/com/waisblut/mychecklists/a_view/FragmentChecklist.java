2438554f2b9750ff266e824b6d48fc597e7aacab
waisblut <ewaisblut@gmail.com>
2015-04-06 19:01:43 -0300

    Moving create_a_test method to Main

--- app/src/main/java/com/waisblut/mychecklists/a_view/FragmentChecklist.java
+++ app/src/main/java/com/waisblut/mychecklists/a_view/FragmentChecklist.java
@@ -18,11 +18,7 @@ import com.nhaarman.listviewanimations.itemmanipulation.DynamicListView;
 import com.nhaarman.listviewanimations.itemmanipulation.dragdrop.OnItemMovedListener;
 import com.waisblut.mychecklists.R;
 import com.waisblut.mychecklists.b_model.Checklist;
-import com.waisblut.mychecklists.b_model.ChecklistItem;
 import com.waisblut.mychecklists.c_data.DSChecklist;
-import com.waisblut.mychecklists.c_data.DSChecklistItem;
-
-import java.util.LinkedList;
 
 
 public class FragmentChecklist
@@ -49,8 +45,6 @@ public class FragmentChecklist
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-        create_a_test();
     }
 
     @Override
@@ -61,8 +55,6 @@ public class FragmentChecklist
 
         setAdapter(adapter);
         setDragAndDrop(adapter);
-
-        mMyListView.setOnItemClickListener(new MyOnItemClickListener(mMyListView));
 
         mMyListView.setSelection(0);
     }
@@ -79,6 +71,7 @@ public class FragmentChecklist
     private void setDragAndDrop(MyListAdapter adapter) {
         mMyListView.enableDragAndDrop();
         //mMyListView.setDraggableManager(new TouchViewDraggableManager(R.id.list_row_draganddrop_touchview));
+        mMyListView.setOnItemClickListener(new MyOnItemClickListener(mMyListView));
         mMyListView.setOnItemMovedListener(new MyOnItemMovedListener(getActivity(), adapter));
         mMyListView.setOnItemLongClickListener(new MyOnItemLongClickListener(mMyListView));
     }
@@ -105,100 +98,6 @@ public class FragmentChecklist
     public void onDestroy() {
         super.onDestroy();
         mListener = null;
-    }
-
-    private void create_a_test() {
-        LinkedList<ChecklistItem> list;
-        ChecklistItem item;
-        DSChecklist dsChecklist = new DSChecklist(getActivity());
-        DSChecklistItem dsChecklistItem = new DSChecklistItem(getActivity());
-
-
-        list = new LinkedList<>();
-        item = new ChecklistItem("Acionar motor", 0);
-        list.add(item);
-        item = new ChecklistItem("Testar turbina esquerda", 1);
-        list.add(item);
-        item = new ChecklistItem("Testar turbina direita", 2);
-        list.add(item);
-        item = new ChecklistItem("Decolar", 3);
-        list.add(item);
-
-        Checklist checklist = new Checklist("Lista de Decolagem", list, 0);
-        dsChecklist.open();
-        dsChecklistItem.open();
-        dsChecklist.create(checklist);
-
-
-        list = new LinkedList<>();
-        item = new ChecklistItem("pegar mala", 0);
-        list.add(item);
-        item = new ChecklistItem("encher de roupa", 1);
-        list.add(item);
-        item = new ChecklistItem("fechar mala", 2);
-        list.add(item);
-
-        checklist = new Checklist("Lista de Viagem", list, 3);
-        dsChecklist.create(checklist);
-
-
-        list = new LinkedList<>();
-        item = new ChecklistItem("pegar vara", 0);
-        list.add(item);
-        item = new ChecklistItem("Testar molinete", 1);
-        list.add(item);
-        item = new ChecklistItem("Testar barco", 2);
-        list.add(item);
-        item = new ChecklistItem("pescar", 3);
-        list.add(item);
-
-        checklist = new Checklist("Lista de Pescaria", list, 2);
-        dsChecklist.create(checklist);
-
-
-        list = new LinkedList<>();
-        item = new ChecklistItem("pegar chuteira", 0);
-        list.add(item);
-        item = new ChecklistItem("pegar bola", 1);
-        list.add(item);
-        item = new ChecklistItem("pegar mochila", 2);
-        list.add(item);
-        item = new ChecklistItem("jogar", 3);
-        list.add(item);
-
-        checklist = new Checklist("Lista de Futebol", list, 1);
-        dsChecklist.create(checklist);
-
-        list = new LinkedList<>();
-        checklist = new Checklist("Lista de Teste1", list, 4);
-        dsChecklist.create(checklist);
-
-        list = new LinkedList<>();
-        checklist = new Checklist("Lista de Teste1.1", list, 5);
-        dsChecklist.create(checklist);
-
-        list = new LinkedList<>();
-        checklist = new Checklist("Lista de Teste2", list, 6);
-        dsChecklist.create(checklist);
-
-        list = new LinkedList<>();
-        checklist = new Checklist("Lista de Teste3", list, 7);
-        dsChecklist.create(checklist);
-
-        list = new LinkedList<>();
-        checklist = new Checklist("Lista de Teste4", list, 8);
-        dsChecklist.create(checklist);
-
-        list = new LinkedList<>();
-        checklist = new Checklist("Lista de Teste5", list, 9);
-        dsChecklist.create(checklist);
-
-        list = new LinkedList<>();
-        checklist = new Checklist("Lista de Teste1", list, 10);
-        dsChecklist.create(checklist);
-
-        dsChecklist.selectWholeTableOnLogCat();
-        dsChecklistItem.selectWholeTableOnLogCat();
     }
 
     public interface onFragmentChecklistListener {
--- app/src/main/java/com/waisblut/mychecklists/a_view/Main.java
+++ app/src/main/java/com/waisblut/mychecklists/a_view/Main.java
@@ -9,6 +9,10 @@ import android.widget.TextView;
 import com.waisblut.mychecklists.R;
 import com.waisblut.mychecklists.b_model.Checklist;
 import com.waisblut.mychecklists.b_model.ChecklistItem;
+import com.waisblut.mychecklists.c_data.DSChecklist;
+import com.waisblut.mychecklists.c_data.DSChecklistItem;
+
+import java.util.LinkedList;
 
 public class Main
         extends Activity
@@ -24,9 +28,10 @@ public class Main
             getFragmentManager().beginTransaction()
                                 .add(R.id.mainContainer, new FragmentChecklist())
                                 .commit();
+
+            create_a_test();
         }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -42,6 +47,101 @@ public class Main
 
     }
 
+    private void create_a_test() {
+        LinkedList<ChecklistItem> list;
+        ChecklistItem item;
+        DSChecklist dsChecklist = new DSChecklist(this);
+        DSChecklistItem dsChecklistItem = new DSChecklistItem(this);
+
+
+        list = new LinkedList<>();
+        item = new ChecklistItem("Acionar motor", 0);
+        list.add(item);
+        item = new ChecklistItem("Testar turbina esquerda", 1);
+        list.add(item);
+        item = new ChecklistItem("Testar turbina direita", 2);
+        list.add(item);
+        item = new ChecklistItem("Decolar", 3);
+        list.add(item);
+
+        Checklist checklist = new Checklist("Lista de Decolagem", list, 0);
+        dsChecklist.open();
+        dsChecklistItem.open();
+        dsChecklist.create(checklist);
+
+
+        list = new LinkedList<>();
+        item = new ChecklistItem("pegar mala", 0);
+        list.add(item);
+        item = new ChecklistItem("encher de roupa", 1);
+        list.add(item);
+        item = new ChecklistItem("fechar mala", 2);
+        list.add(item);
+
+        checklist = new Checklist("Lista de Viagem", list, 3);
+        dsChecklist.create(checklist);
+
+
+        list = new LinkedList<>();
+        item = new ChecklistItem("pegar vara", 0);
+        list.add(item);
+        item = new ChecklistItem("Testar molinete", 1);
+        list.add(item);
+        item = new ChecklistItem("Testar barco", 2);
+        list.add(item);
+        item = new ChecklistItem("pescar", 3);
+        list.add(item);
+
+        checklist = new Checklist("Lista de Pescaria", list, 2);
+        dsChecklist.create(checklist);
+
+
+        list = new LinkedList<>();
+        item = new ChecklistItem("pegar chuteira", 0);
+        list.add(item);
+        item = new ChecklistItem("pegar bola", 1);
+        list.add(item);
+        item = new ChecklistItem("pegar mochila", 2);
+        list.add(item);
+        item = new ChecklistItem("jogar", 3);
+        list.add(item);
+
+        checklist = new Checklist("Lista de Futebol", list, 1);
+        dsChecklist.create(checklist);
+
+        list = new LinkedList<>();
+        checklist = new Checklist("Lista de Teste1", list, 4);
+        dsChecklist.create(checklist);
+
+        list = new LinkedList<>();
+        checklist = new Checklist("Lista de Teste1.1", list, 5);
+        dsChecklist.create(checklist);
+
+        list = new LinkedList<>();
+        checklist = new Checklist("Lista de Teste2", list, 6);
+        dsChecklist.create(checklist);
+
+        list = new LinkedList<>();
+        checklist = new Checklist("Lista de Teste3", list, 7);
+        dsChecklist.create(checklist);
+
+        list = new LinkedList<>();
+        checklist = new Checklist("Lista de Teste4", list, 8);
+        dsChecklist.create(checklist);
+
+        list = new LinkedList<>();
+        checklist = new Checklist("Lista de Teste5", list, 9);
+        dsChecklist.create(checklist);
+
+        list = new LinkedList<>();
+        checklist = new Checklist("Lista de Teste1", list, 10);
+        dsChecklist.create(checklist);
+
+        dsChecklist.selectWholeTableOnLogCat();
+        dsChecklistItem.selectWholeTableOnLogCat();
+    }
+
+    //region Interface Methods
     @Override
     public void onClickChecklist(Checklist checklist) {
         TextView txtPosition;
@@ -67,4 +167,5 @@ public class Main
     public void onLongClickChecklistItem(int newId, int oldId) {
 
     }
+    //endregion
 }
